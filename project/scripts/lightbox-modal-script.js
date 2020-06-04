cf8796a716e3af2dcfb6468478ce47d6e7d1aeb5
ElqBell <silenc8@gmail.com>
2020-06-04 16:52:28 +0300

    created thumbnail images horizontal scroll

--- project/scripts/lightbox-modal-script.js
+++ project/scripts/lightbox-modal-script.js
@@ -11,16 +11,57 @@ function closeModal() {
 }
     
 let slideIndex = 1;
+let scrollI = 0;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
+    const divCount = document.getElementsByClassName("lightbox-thumbnail").length;
+    if(n > divCount)
+        scrollI = 0;
+    else if(n === - 1) {
+        if(scrollI === 0)
+            scrollI = divCount - 1;
+        else
+            scrollI--;
+    }
+    else 
+        scrollI++;
     showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
+    const row = document.getElementsByClassName("scrolling-thumbnail-row")[0];
+    const rowStyle = getComputedStyle(row);
+    const rowWidth = parseInt(rowStyle.getPropertyValue('width'));
+    const lengthOfOneDiv = row.scrollWidth / document.getElementsByClassName("lightbox-thumbnail").length;
+    const thumbnails = document.getElementsByClassName("lightbox-thumbnail");
+    let countSideDivs = 0;
+    for(let i = 0; i < thumbnails.length; i++)
+        if(lengthOfOneDiv * i < rowWidth / 2)
+            countSideDivs++;
+    
+    if(scrollI = n - countSideDivs);
     showSlides(slideIndex = n);
+}
+
+function scrollThumbnailRow(thumbnails) {
+    const row = document.getElementsByClassName("scrolling-thumbnail-row")[0];
+    const rowStyle = getComputedStyle(row);
+    const rowWidth = parseInt(rowStyle.getPropertyValue('width'));
+    const lengthOfOneDiv = row.scrollWidth / thumbnails.length;
+    for(let i = 0; i < thumbnails.length; i++) {
+        if(thumbnails[i].classList.contains("active")) {
+            if(lengthOfOneDiv * i < rowWidth / 2) {
+                row.scrollLeft = 0;
+                scrollI = 0;
+            }
+            else {
+                row.scrollLeft = lengthOfOneDiv * scrollI;
+            }
+        }
+    }
 }
 
 function showSlides(n) {
@@ -38,11 +79,5 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "flex";
     slides[slideIndex-1].classList.add("active");
     thumbnails[slideIndex-1].classList.add("active");
+    scrollThumbnailRow(thumbnails);
 }
-
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target.id === "myModal") {
-//         e.target.style.display = "none";
-//     }
-//   }
