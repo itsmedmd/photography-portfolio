e29ab1e87c206adc092768c5854cbe86657e8c51
ElqBell <silenc8@gmail.com>
2020-06-26 15:03:33 +0300

    made video gallery work for internet explorer

--- project/scripts/add-iframe-src.js
+++ project/scripts/add-iframe-src.js
@@ -1,11 +1,19 @@
 function addSrcToIframes() {
-    const iframes = Array.from(document.getElementsByClassName('responsive-iframe'));
-    iframes.forEach(function(iframe) {
-        // dataset.src is for adding "data-src" attribute
+    const iframes = document.getElementsByClassName('responsive-iframe');
+
+    // Changing HTMLCollection to array this way instead of
+    // Array.from because internet explorer does not support Array.from
+    const temp_array = [], length = iframes.length;
+    for (var i = 0; i < length; i++)
+        temp_array.push(iframes[i]);
+
+    temp_array.forEach(function(iframe) {
         iframe.dataset.src = "https://www.youtube.com/embed/mZ2zy-wnR0s?controls=1&rel=0";
     });
 };
 
+// adding the lazyload script after the iframes have rendered
+// because otherwise it doesn't work
 function addLazyloadScript() {
     const body = document.getElementsByTagName('body')[0];
     const js = document.createElement("script");
