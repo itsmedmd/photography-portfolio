f1e517afa7aa5815b6029a2883df83167688b27a
ElqBell <silenc8@gmail.com>
2020-06-27 15:46:26 +0300

    remade add-iframe-src to work with aws lambda

--- project/scripts/add-iframe-src.js
+++ project/scripts/add-iframe-src.js
@@ -1,36 +1,21 @@
-function addSrcToIframes() {
+function addSrcToIframes(videoURLs) {
     const iframes = document.getElementsByClassName('responsive-iframe');
-
+    
     // Changing HTMLCollection to array this way instead of
     // Array.from because internet explorer does not support Array.from
     const temp_array = [], length = iframes.length;
-    for (var i = 0; i < length; i++)
+    for (let i = 0; i < length; i++)
         temp_array.push(iframes[i]);
 
-    temp_array.forEach(function(iframe) {
-        iframe.dataset.src = "https://www.youtube.com/embed/mZ2zy-wnR0s?controls=1&rel=0";
-    });
+    for (let i = 0; i < length; i++)
+        temp_array[i].dataset.src = videoURLs[i];
 };
 
 // adding the lazyload script after the iframes have rendered
 // because otherwise it doesn't work
-function addLazyloadScript() {
+function addLazyloadScript(source) {
     const body = document.getElementsByTagName('body')[0];
     const js = document.createElement("script");
-    js.src = 'scripts/lazyload.js';
+    js.src = source;
     body.appendChild(js);
-}
-
-// at 768px and less the header shows only hamburger bar
-// for navigation, where facebook like button embed is,
-// which is why loading youtube iframes is prioritised here
-if(screen.availWidth <= 768) {
-    addSrcToIframes();
-    addLazyloadScript();
-} else {
-    // first loads facebook like button (with the whole page)
-    window.onload = function() {
-        addSrcToIframes();
-        addLazyloadScript();
-    };
 }
