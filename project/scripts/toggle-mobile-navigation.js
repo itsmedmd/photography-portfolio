c1dd64c1d00205b52b523ced8330bf2be6421b55
ElqBell <silenc8@gmail.com>
2020-07-16 16:20:53 +0300

    when nav is on and screen is resized above 768px, nav gets turned to desktop mode

--- project/scripts/toggle-mobile-navigation.js
+++ project/scripts/toggle-mobile-navigation.js
@@ -1,21 +1,35 @@
-function toggleMobileNavigation() {
+function turnNavigationOn(pageContent) {
     const nav = document.getElementById("top-navigation");
     const socials = document.getElementById("top-social-media-icons");
-    const pageContent = document.getElementsByClassName("page-content")[0];
-    if(pageContent.classList.contains("displayOff")) {
-        nav.classList.remove("mobile-navigation-visible");
-        nav.classList.add("mobile-navigation-hidden");
-        socials.classList.remove("mobile-socials-visible");
-        socials.classList.add("mobile-socials-hidden");
-        pageContent.classList.remove("displayOff");
-        document.getElementsByTagName("html")[0].classList.remove("scroll-lock");
-    }
-    else {
-        pageContent.classList.add("displayOff");
-        nav.classList.remove("mobile-navigation-hidden");
-        nav.classList.add("mobile-navigation-visible");
-        socials.classList.remove("mobile-socials-hidden");
-        socials.classList.add("mobile-socials-visible");
-        document.getElementsByTagName("html")[0].classList.add("scroll-lock");
-    }
+    pageContent.classList.add("displayOff");
+    nav.classList.remove("mobile-navigation-hidden");
+    nav.classList.add("mobile-navigation-visible");
+    socials.classList.remove("mobile-socials-hidden");
+    socials.classList.add("mobile-socials-visible");
+    document.getElementsByTagName("html")[0].classList.add("scroll-lock");
 }
+
+function turnNavigationOff(pageContent) {
+    const nav = document.getElementById("top-navigation");
+    const socials = document.getElementById("top-social-media-icons");
+    nav.classList.remove("mobile-navigation-visible");
+    nav.classList.add("mobile-navigation-hidden");
+    socials.classList.remove("mobile-socials-visible");
+    socials.classList.add("mobile-socials-hidden");
+    pageContent.classList.remove("displayOff");
+    document.getElementsByTagName("html")[0].classList.remove("scroll-lock");
+}
+
+function toggleMobileNavigation() {
+    const pageContent = document.getElementsByClassName("page-content")[0];
+    if(pageContent.classList.contains("displayOff"))
+        turnNavigationOff(pageContent);
+    else
+        turnNavigationOn(pageContent);
+}
+
+window.addEventListener('resize', () => {
+    const pageContent = document.getElementsByClassName("page-content")[0];
+    if(screen.availWidth > 768 && pageContent.classList.contains("displayOff"))
+        turnNavigationOff(pageContent);
+});
